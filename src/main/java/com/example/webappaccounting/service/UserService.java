70512e8c947e73c7385cb401f20661129d9ed891
vskotelnikov <val.kotelnikov@mail.ru>
2022-11-07 09:26:42 +0300

    Add bootstrap

--- src/main/java/com/example/webappaccounting/service/UserService.java
+++ src/main/java/com/example/webappaccounting/service/UserService.java
@@ -1,11 +1,15 @@
 package com.example.webappaccounting.service;
 
+import com.example.webappaccounting.model.Role;
+import com.example.webappaccounting.model.User;
 import com.example.webappaccounting.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,5 +21,18 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDB !=null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return true;
     }
 }
